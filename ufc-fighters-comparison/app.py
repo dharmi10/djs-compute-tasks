27d2fc2369dd519c491e572364e49ba3eaf389a3
dharmi10 <dharmi.chauhan10@gmail.com>
2025-09-03 09:03:17 +0530

    Update app.py

--- ufc-fighters-comparison/app.py
+++ ufc-fighters-comparison/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-
-data = pd.read_csv(r'C:/Users/User/OneDrive/Desktop/ufc-fighters-comparison')
+data = pd.read_csv(r'C:/Users/User/OneDrive/Desktop/ufc-fighters-statistics-CLEANED')
 st.set_page_config(page_title="Fighter Comparison", layout="wide")
 
 st.title("🥊 UFC Fighter Comparison App")
@@ -90,3 +89,4 @@ st.dataframe(
     data[data['name'].isin([fighter1, fighter2])][['name', 'wins', 'losses', 'draws']].fillna(0)
 
 )
+
